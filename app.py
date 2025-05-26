222a8ad817208895bb4fc2f4a533d0718e98c1bc
Harish Aggarwal <harishaggarwal2516@yahoo.com>
2025-05-26 13:36:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -256,8 +256,6 @@ else:
 
 if __name__ == "__main__":
     demo.launch(
-        server_name="127.0.0.1",
         server_port=7860,
-        share=True,
-        debug=True
+        debug=False  # optional
     )
